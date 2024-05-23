d69f92e14015928cfd4b607d3e669f2b2fa28c60
sbrom3630 <shaulbromberg@gmail.com>
2024-05-23 14:01:51 -0400

    add hw 51 files

---
+++ html/51/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace C_Assesment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] patterns = new string[] { "striped", "plaid", "plain" };
+            string[] colors = new string[] {"Black", "Red", "Green", "Blue"};
+            for (int i = 0; i < colors.Length; i++)
+            {
+                for (int j = 0; j < patterns.Length; j++)
+                {
+                    var shirt = new Shirt(colors[i], patterns[j]);
+                    Console.WriteLine(shirt);
+                }
+            }
+        }
+    }
+}
---
+++ html/51/Shirt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Assesment
+{
+    internal class Shirt
+    {
+        public string ShirtColor { get; set; }
+        public string ShirtPattern { get; set; }
+
+        public Shirt(string shirtColor, string shirtPattern)
+        {
+            ShirtColor = shirtColor;
+            ShirtPattern = shirtPattern;
+        }
+
+        public override string? ToString()
+        {
+            return $"This is a {ShirtColor} {ShirtPattern} shirt";
+        }
+    }
+}
